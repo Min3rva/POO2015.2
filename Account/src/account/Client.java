ac43fb9c069182f8d8058278ae267e640a5d779a
Talita <talita.v.matta@gmail.com>
2015-11-24 01:19:43 -0300

    Adicionando projeto Account

---
+++ Account/src/account/Client.java
@@ -0,0 +1,33 @@
+package account;
+
+public class Client {
+	String name;
+	String address;
+	
+	Client(String n, String a){
+		//TODO
+	}
+	
+	String getName(){
+		//TODO
+		return null;
+	}
+	
+	String getAddress(){
+		//TODO
+		return null;
+	}
+	
+	void setName(String n){
+		//TODO
+	}
+	
+	void setAddress(String a){
+		//TODO
+	}
+	
+	public String toString(){
+		//TODO
+		return null;
+	}
+}
---
+++ Account/src/account/Conta.java
@@ -0,0 +1,24 @@
+package account;
+
+public class Conta {
+	String number;
+	Client client;
+	int saldo;
+	
+	Conta (String num){
+		//TODO
+	}
+	
+	Conta (String num, Client cl){
+		//TODO
+	}
+	
+	Client getClient(){
+		//TODO
+		return null;
+	}
+	
+	void setClient(Client cl){
+		//TODO
+	}
+}
